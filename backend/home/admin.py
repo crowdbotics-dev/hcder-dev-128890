245424237245201dd645f189740b2c6444b02b6d
crowdbotics-dev <engineering@crowdbotics.com>
2023-11-14 06:25:03 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Xcvsb
+
+admin.site.register(Xcvsb)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,8 @@
+from django.conf import settings
 from django.db import models
+
+
+class Xcvsb(models.Model):
+    "Generated Model"
+    fhds = models.BigIntegerField()
+    teru = models.BigIntegerField()
